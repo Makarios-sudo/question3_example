20a824143934815dddf66bd257e26cd13759b612
Michael Thompson <michaelthom178@gmail.comgit config --global user.name makarios>
2024-08-06 09:25:30 +0100

    first commit

---
+++ src/components/ListPosts.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const ListPosts = () => {
+  // setting the values of post to an empty list
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // using axios to call the json placeholder endpoint
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <ul>
+        {posts.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListPosts;
